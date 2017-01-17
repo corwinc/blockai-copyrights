2acce04dc1b410a7f6d64c5724d8c8c208de96bc
Corwin Crownover <corwin.crownover@gmail.com>
2017-01-17 13:52:15 -0800

    Create dummy pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,6 +4,23 @@ const Pagination = function() {
 	return (
 		<div>
 			<div id='pagination-container'>pagination</div>
+			<ul className="pagination">
+				<li className="page-item">
+				  <a className="page-link" href="#" aria-label="Previous">
+				    <span aria-hidden="true">&laquo;</span>
+				    <span className="sr-only">Previous</span>
+				  </a>
+				</li>
+				<li className="active"><a className="page-link" href="#">1</a></li>
+				<li className="page-item"><a className="page-link" href="#">2</a></li>
+				<li className="page-item"><a className="page-link" href="#">3</a></li>
+				<li className="page-item">
+				  <a className="page-link" href="#" aria-label="Previous">
+				    <span aria-hidden="true">&raquo;</span>
+				    <span className="sr-only">Previous</span>
+				  </a>
+				</li>
+			</ul>
 		</div>
 	);
 }
