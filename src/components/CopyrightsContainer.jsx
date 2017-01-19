c63ab83d861463bb43bd46def802b7f018247647
Corwin Crownover <corwin.crownover@gmail.com>
2017-01-18 17:58:46 -0800

    Hook up react-bootstrap pagination to data

--- src/components/CopyrightsContainer.jsx
+++ src/components/CopyrightsContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render, connect} from 'react-dom';
 
-import Pagination from './Pagination.jsx';
+import {Pagination} from 'react-bootstrap';
 import TileGrid from './TileGrid.jsx';
 import Tile from './Tile.jsx';
 import * as actions from '../actions/index.js';
@@ -264,18 +264,86 @@ export default class CopyrightsContainer extends React.Component {
 					img: 'https://files.blockai.com/v1/transform/thumb/bKmwXQpQ41G7',
 					title: 'Jan copyright',
 					desc: 'Published on instagram also!'
+				},
+				{
+					user: 'Corwin',
+					avatar: 'https://files.blockai.com/v1/files/KdGNObx2n12z+avatar.png',
+					img: 'https://files.blockai.com/v1/transform/thumb/bKmwXQpQ41G7',
+					title: 'My copyright',
+					desc: 'Published on instagram.'
+				},
+				{
+					user: 'Jan',
+					avatar: 'https://files.blockai.com/v1/files/KdGNObx2n12z+avatar.png',
+					img: 'https://files.blockai.com/v1/transform/thumb/bKmwXQpQ41G7',
+					title: 'Jan copyright',
+					desc: 'Published on instagram also!'
+				},
+				{
+					user: 'Corwin',
+					avatar: 'https://files.blockai.com/v1/files/KdGNObx2n12z+avatar.png',
+					img: 'https://files.blockai.com/v1/transform/thumb/bKmwXQpQ41G7',
+					title: 'My copyright',
+					desc: 'Published on instagram.'
+				},
+				{
+					user: 'Jan',
+					avatar: 'https://files.blockai.com/v1/files/KdGNObx2n12z+avatar.png',
+					img: 'https://files.blockai.com/v1/transform/thumb/bKmwXQpQ41G7',
+					title: 'Jan copyright',
+					desc: 'Published on instagram also!'
+				},
+				{
+					user: 'Corwin',
+					avatar: 'https://files.blockai.com/v1/files/KdGNObx2n12z+avatar.png',
+					img: 'https://files.blockai.com/v1/transform/thumb/bKmwXQpQ41G7',
+					title: 'My copyright',
+					desc: 'Published on instagram.'
+				},
+				{
+					user: 'Jan',
+					avatar: 'https://files.blockai.com/v1/files/KdGNObx2n12z+avatar.png',
+					img: 'https://files.blockai.com/v1/transform/thumb/bKmwXQpQ41G7',
+					title: 'Jan copyright',
+					desc: 'Published on instagram also!'
+				},
+				{
+					user: 'Corwin',
+					avatar: 'https://files.blockai.com/v1/files/KdGNObx2n12z+avatar.png',
+					img: 'https://files.blockai.com/v1/transform/thumb/bKmwXQpQ41G7',
+					title: 'My copyright',
+					desc: 'Published on instagram.'
+				},
+				{
+					user: 'Jan',
+					avatar: 'https://files.blockai.com/v1/files/KdGNObx2n12z+avatar.png',
+					img: 'https://files.blockai.com/v1/transform/thumb/bKmwXQpQ41G7',
+					title: 'Jan copyright',
+					desc: 'Published on instagram also!'
 				}
-			]
+
+			],
+			activePage: 1
 		};
 
+		this.onSelect = this.onSelect.bind(this);
 		this.getCopyrights = this.getCopyrights.bind(this);
 		this.setCopyrights = this.setCopyrights.bind(this);
 		this.renderCopyrightTiles = this.renderCopyrightTiles.bind(this);
 	}
 
+	componentDidMount() {
+		this.getCopyrights();
+	}
+
+	onSelect(number) {
+	  	this.setState({activePage: number});
+	}
+
 	getCopyrights() {
+		console.log('inside getCopyrights');
 		// get request
-		// setCopyrights(response);
+		// then: setCopyrights(response);
 	}
 
 	setCopyrights(copyrights) {
@@ -283,7 +351,12 @@ export default class CopyrightsContainer extends React.Component {
 	}
 
 	renderCopyrightTiles() {
-		var copyrightTiles = this.state.copyrights.map((cr, i) => {
+		var tilesStart = (this.state.activePage - 1) * 36;
+		var tilesEnd = this.state.activePage * 36;
+		console.log('tilesStart:', tilesStart);
+		var selectTiles = this.state.copyrights.slice(tilesStart, tilesEnd);
+		console.log('selectTiles:', selectTiles);
+		var copyrightTiles = selectTiles.map((cr, i) => {
 			return (
 				<Tile key={i} copyright={cr} />
 			)
@@ -293,9 +366,20 @@ export default class CopyrightsContainer extends React.Component {
 	}
 
 	render() {
+		var pages = Math.ceil(this.state.copyrights.length / 36);
+
 		return (
 			<div className={styles.copyrightsContainer}>
-				<Pagination className={styles.paginationContainerTop}/>
+				<Pagination 
+					prev
+					next
+					first
+					last
+					items={pages}
+					activePage={this.state.activePage}
+					onSelect={this.onSelect}
+					maxButtons={5}
+		        />
 				<TileGrid renderCopyrightTiles={this.renderCopyrightTiles} copyrights={this.state.copyrights}/>
 			</div>
 		);
